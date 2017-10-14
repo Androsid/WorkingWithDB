003edb242bd7e8eea8e3e1f8aab1fae8de93a00b
Андрей Сидоров <Andro_oi@mail.ru>
2017-10-14 20:35:00 +0300

    Добавьте файлы проекта.

---
+++ WorkingWithDB/WorkingWithDB.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkingWithDB
+{
+    
+    public partial class WorkingWithDB : Form
+    {
+        SqlConnection sqlConnection;
+
+        public WorkingWithDB()
+        {
+            InitializeComponent();
+        }
+
+        private async void WorkingWithDB_Load(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Andro\source\repos\WorkingWithDB\WorkingWithDB\Database.mdf;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            await sqlConnection.OpenAsync();
+
+            updateDB();
+        }
+
+        private async void updateDB() // for automatically update database
+        {
+            listBox1.Items.Clear();
+            SqlDataReader sqlReader = null;
+            SqlCommand command = new SqlCommand("SELECT * FROM [Products]", sqlConnection);
+
+            try
+            {
+                sqlReader = await command.ExecuteReaderAsync();
+                while (await sqlReader.ReadAsync())
+                {
+                    listBox1.Items.Add(Convert.ToString(sqlReader["id"]) + " " + Convert.ToString(sqlReader["Name"]) + " " + Convert.ToString(sqlReader["Price"]));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sqlConnection != null && sqlConnection.State != ConnectionState.Closed)
+                sqlConnection.Close();
+        }
+
+        private void WorkingWithDB_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (sqlConnection != null && sqlConnection.State != ConnectionState.Closed)
+                sqlConnection.Close();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (label7.Visible)
+                label7.Visible = false;
+
+            if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox1.Text) &&
+                !string.IsNullOrEmpty(textBox2.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO [Products] (Name, Price)VALUES(@Name, @Price)", sqlConnection);
+
+                command.Parameters.AddWithValue("Name", textBox1.Text);
+                command.Parameters.AddWithValue("Price", textBox2.Text);
+
+                await command.ExecuteNonQueryAsync();
+                updateDB();
+            }
+            else
+            {
+                label7.Visible = true;
+                label7.Text = "Not Valid fields ProductName and Costs";
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (label8.Visible)
+                label8.Visible = false;
+
+            if (!string.IsNullOrEmpty(textBox5.Text) && !string.IsNullOrWhiteSpace(textBox5.Text) &&
+                !string.IsNullOrEmpty(textBox4.Text) && !string.IsNullOrWhiteSpace(textBox4.Text) &&
+                !string.IsNullOrEmpty(textBox3.Text) && !string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                SqlCommand command = new SqlCommand("UPDATE [Products] SET [Name]=@Name, [Price]=@Price WHERE [Id]=@Id", sqlConnection);
+
+                command.Parameters.AddWithValue("Name", textBox4.Text);
+                command.Parameters.AddWithValue("Price", textBox3.Text);
+                command.Parameters.AddWithValue("Id", textBox5.Text);
+
+                await command.ExecuteNonQueryAsync();
+                updateDB();
+            }
+            else if (string.IsNullOrEmpty(textBox5.Text) && string.IsNullOrWhiteSpace(textBox5.Text))
+            {
+                label8.Visible = true;
+                label8.Text = "Not Valid Id";
+            }
+            else
+            {
+                label8.Visible = true;
+                label8.Text = "Not Valid fields ProductName and Costs";
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (label9.Visible)
+                label9.Visible = false;
+
+            if (!string.IsNullOrEmpty(textBox6.Text) && !string.IsNullOrWhiteSpace(textBox6.Text))
+            {
+                SqlCommand command = new SqlCommand("DELETE FROM [Products] WHERE [Id]=@Id", sqlConnection);
+                command.Parameters.AddWithValue("Id", textBox6.Text);
+                await command.ExecuteNonQueryAsync();
+                updateDB();
+            }
+            else
+            {
+                label9.Visible = true;
+                label9.Text = "Not Valid Id";
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Trololo it's made by Andro!");
+        }
+    }
+}
